203b9b0c477764ec3ab4e6411e60c675d26dbf7f
Raksa Adhinata <raksaadhinata@gmail.com>
2026-03-14 03:17:39 +0700

    Update ingestion_oil.py

--- src/pipeline/ingestion_oil.py
+++ src/pipeline/ingestion_oil.py
@@ -16,8 +16,8 @@ def fetch_oil_brent(api_key, frequency, start_date, end_date, offset=0, length=5
     params = {
         "api_key"      :      api_key,
         "frequency"    :      frequency,
-        "start_date"   :      "2010-01-01",
-        "end_date"     :      "end_date",
+        "start"        :      "2010-01-01",
+        "end"          :      end_date,
         "data[0]"      :      "value",
         "facets[series][]"  : "EPCBRENT",
         "sort[0][column]"   : "period",

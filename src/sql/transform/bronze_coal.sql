520ca25b88983da79802fc5217d7c3a931f9c66c
Raksa Adhinata <raksaadhinata@gmail.com>
2026-03-17 22:09:55 +0700

    Update bronze_coal.sql

--- src/sql/transform/bronze_coal.sql
+++ src/sql/transform/bronze_coal.sql
@@ -1,1 +1,14 @@
+INSERT INTO bronze_coal_price
 
+SELECT
+    CAST(date AS DATE)            AS observation_date,
+    CAST(value AS DOUBLE)         AS price,
+    CAST(realtime_start AS DATE)  AS realtime_start,
+    CAST(realtime_end AS DATE)    AS realtime_end,
+    CURRENT_TIMESTAMP             AS ingestion_timestamp
+
+FROM read_parquet(
+'s3://scg-energy-analytics-data/bronze/energy/coal/year=2026/month=03/day=13/20260313T201006Z.parquet'
+)
+
+WHERE value IS NOT NULL;

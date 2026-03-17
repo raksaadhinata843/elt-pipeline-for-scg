f8c5890d5258561f6de681320165f96e061d53b7
Raksa Adhinata <raksaadhinata@gmail.com>
2026-03-17 22:29:26 +0700

    Update bronze_oil.sql

--- src/sql/transform/bronze_oil.sql
+++ src/sql/transform/bronze_oil.sql
@@ -1,8 +1,9 @@
-INSERT INTO bronze_oil_price
+MERGE INTO bronze_oil_price AS target
+USING (
 
 SELECT
-    CAST(date AS DATE)        AS observation_date,
-    CAST(value AS DOUBLE)     AS price,
+    CAST(date AS DATE) AS observation_date,
+    CAST(value AS DOUBLE) AS price
     units,
     series,
     CURRENT_TIMESTAMP         AS ingestion_timestamp
@@ -11,4 +12,13 @@ FROM read_parquet(
 's3://scg-energy-analytics-data/bronze/energy/brent/year=2026/month=03/day=14/20260314T210045Z.parquet'
 )
 
-WHERE value IS NOT NULL;
+) AS source
+
+ON target.observation_date = source.observation_date
+
+WHEN MATCHED THEN
+UPDATE SET price = source.price
+
+WHEN NOT MATCHED THEN
+INSERT (observation_date, price)
+VALUES (source.observation_date, source.price);

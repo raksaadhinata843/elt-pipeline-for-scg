17f892e6458dad7900b95fd840464366f567808b
Raksa Adhinata <raksaadhinata@gmail.com>
2026-03-18 00:53:06 +0700

    Update bronze_oil.sql

--- src/sql/transform/bronze_oil.sql
+++ src/sql/transform/bronze_oil.sql
@@ -6,5 +6,5 @@ INSERT INTO bronze_oil_price
         series,
         CURRENT_TIMESTAMP         AS ingestion_timestamp
    FROM read_parquet(
-    's3://scg-energy-analytics-data/bronze/energy/coal/year=2026/month=03/day=17/20260317T162410Z.parquet'
+    's3://scg-energy-analytics-data/bronze/energy/brent/year=2026/month=03/day=17/20260317T162408Z.parquet'
     );

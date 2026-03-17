112cdd667e366ad347e89b538b629ec27203efd3
Raksa Adhinata <raksaadhinata@gmail.com>
2026-03-18 00:00:56 +0700

    Update bronze_cement.sql

--- src/sql/transform/bronze_cement.sql
+++ src/sql/transform/bronze_cement.sql
@@ -1,17 +1,14 @@
 MERGE INTO bronze_cement_price AS target
 USING (
-
-SELECT
-    CAST(date AS DATE) AS observation_date,
-    CAST(value AS DOUBLE) AS price
-    CAST(realtime_start AS DATE)  AS realtime_start,
-    CAST(realtime_end AS DATE)    AS realtime_end,
-    CURRENT_TIMESTAMP             AS ingestion_timestamp
-
-
-FROM read_parquet(
-'s3://scg-energy-analytics-data/bronze/construction/cement/year=2026/month=03/day=13/20260313T201009Z.parquet'
-)
+    SELECT
+        CAST(date AS DATE) AS observation_date,
+        CAST(value AS DOUBLE) AS price
+        CAST(realtime_start AS DATE)  AS realtime_start,
+        CAST(realtime_end AS DATE)    AS realtime_end,
+        CURRENT_TIMESTAMP             AS ingestion_timestamp
+    FROM read_parquet(
+    's3://scg-energy-analytics-data/bronze/construction/cement/year=2026/month=03/day=13/20260313T201009Z.parquet'
+    )
 
 ) AS source
 

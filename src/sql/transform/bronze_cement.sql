d9173f280110d7ca7782eb849ba6f9f4d0a090f0
Raksa Adhinata <raksaadhinata@gmail.com>
2026-03-17 22:10:40 +0700

    Update bronze_cement.sql

--- src/sql/transform/bronze_cement.sql
+++ src/sql/transform/bronze_cement.sql
@@ -1,1 +1,14 @@
+INSERT INTO bronze_cement_price
 
+SELECT
+    CAST(date AS DATE)            AS observation_date,
+    CAST(value AS DOUBLE)         AS price,
+    CAST(realtime_start AS DATE)  AS realtime_start,
+    CAST(realtime_end AS DATE)    AS realtime_end,
+    CURRENT_TIMESTAMP             AS ingestion_timestamp
+
+FROM read_parquet(
+'s3://scg-energy-analytics-data/bronze/construction/cement/year=2026/month=03/day=13/20260313T201009Z.parquet'
+)
+
+WHERE value IS NOT NULL;

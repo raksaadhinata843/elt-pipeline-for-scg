e8c9154cd0cfb76ee81cb13cc4b28323232d274a
Raksa Adhinata <raksaadhinata@gmail.com>
2026-03-17 22:07:36 +0700

    Update bronze_cement.sql

--- src/sql/ddl/bronze_cement.sql
+++ src/sql/ddl/bronze_cement.sql
@@ -1,1 +1,7 @@
-
+CREATE TABLE IF NOT EXISTS bronze_cement_price (
+    observation_date DATE,
+    price DOUBLE,
+    realtime_start DATE,
+    realtime_end DATE,
+    ingestion_timestamp TIMESTAMP
+);

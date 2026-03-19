cbb9f779dd1613ed1357d40d029c838344048c90
Raksa Adhinata <raksaadhinata@gmail.com>
2026-03-19 21:56:09 +0700

    Delete src/sql directory

--- src/sql/ddl/bronze_coal.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS bronze_coal_price (
-    observation_date DATE,
-    price DOUBLE,
-    realtime_start DATE,
-    realtime_end DATE,
-    ingestion_timestamp TIMESTAMP
-);
--- src/sql/ddl/bronze_oil.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS bronze_oil_price (
-    observation_date DATE,
-    price DOUBLE,
-    units VARCHAR,
-    series VARCHAR,
-    ingestion_timestamp TIMESTAMP
-);

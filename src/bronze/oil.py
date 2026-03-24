d2c8c54ebe1d918097aa3d32d7e376b9fff50e7d
Raksa Adhinata <raksaadhinata@gmail.com>
2026-03-24 14:37:08 +0700

    Update oil.py

--- src/bronze/oil.py
+++ src/bronze/oil.py
@@ -1,1 +1,81 @@
+import os
+import io
+import logging
+import pandas as pd
+import awswrangler as wr
+import requests
+from datetime import datetime, timedelta, timezone
 
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+EIA_URL = "https://api.eia.gov/v2/petroleum/pri/spt/data/"
+
+
+def fetch_oil_brent(api_key, frequency, start_date, end_date, offset=0, length=5000):
+    params = {
+        "api_key"      :      api_key,
+        "frequency"    :      frequency,
+        "start"        :      start_date,
+        "end"          :      end_date,
+        "data[0]"      :      "value",
+        "facets[series][]"  : "RBRTE",
+        "sort[0][column]"   : "period",
+        "sort[0][direction]": "asc",
+        "offset":             offset,
+        "length":             length,
+    }
+
+    resp = requests.get(EIA_URL, params=params, timeout=30)
+    resp.raise_for_status()
+
+    payload = resp.json()
+    logger.info(f"Full API Response {payload}")    
+    
+    data    = payload["response"]["data"]
+    total   = int(payload["response"]["total"])
+    
+    logger.info(f"Fetched {len(data)} records (offset={offset}, total={total})")
+
+    if offset + length < total:
+        data += fetch_oil_brent(api_key, frequency, start_date, end_date, offset + length, length)
+
+    return data
+
+
+def lambda_handler(event, context):
+    api_key   = os.environ["EIA_API_KEY"]
+    bucket    = os.environ["S3_BUCKET"]
+    prefix    = os.environ.get("S3_PREFIX", "bronze/energy/brent")
+    frequency = os.environ.get("EIA_FREQUENCY", "daily")
+    full_load = os.environ.get("FULL_LOAD", True)
+
+    now = datetime.now(timezone.utc)         
+    start_date = "2010-01-01"
+    end_date = now.strftime("%Y-%m-%d")
+    
+    raw = fetch_oil_brent(api_key, frequency, start_date, end_date)
+    if not raw:
+        print("API returned empty response")
+        return {"statusCode": 200, "body": "No data returned from API"}
+
+
+    df = pd.DataFrame(raw)
+    df["value"] = pd.to_numeric(df["value"], errors='coerce')
+    df["date"] = pd.to_datetime(df["period"])
+
+    s3_key = (
+        f"s3://{bucket}/{prefix}/"
+        f"year={now.year}/month={now.month:02d}/day={now.day:02d}/"
+        f"bronze_oil.parquet"
+    )
+    
+    wr.s3.to_parquet(
+        df=df,
+        path=s3_key,
+        index=False
+    )
+  
+    logger.info(f"Uploaded to {s3_key}")
+    
+    return {"statusCode": 200, "records": len(raw), "s3_key": s3_key}

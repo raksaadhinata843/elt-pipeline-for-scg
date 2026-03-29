c98fbb887be4ec21024a0317aa14590b5ffbcf94
Raksa Adhinata <raksaadhinata@gmail.com>
2026-03-30 04:15:51 +0700

    Create gold_lag table with lagged index values
    Create a new table 'gold_lag' with lagged values for coal and oil indices.

--- src/gold/lag.sql
+++ src/gold/lag.sql
@@ -1,1 +1,18 @@
-
+CREATE OR REPLACE TABLE gold_lag AS
+SELECT * FROM (
+  SELECT 
+        obs_month,
+        cement_idx,
+        -- Lag Batubara
+        LAG(coal_idx, 1) OVER(ORDER BY obs_month) AS coal_lag1,
+        LAG(coal_idx, 2) OVER(ORDER BY obs_month) AS coal_lag2,
+        LAG(coal_idx, 3) OVER(ORDER BY obs_month) AS coal_lag3,
+        LAG(coal_idx, 4) OVER(ORDER BY obs_month) AS coal_lag4,
+        -- Lag Minyak
+        LAG(oil_idx, 1) OVER(ORDER BY obs_month) AS oil_lag1,
+        LAG(oil_idx, 2) OVER(ORDER BY obs_month) AS oil_lag2,
+        LAG(oil_idx, 3) OVER(ORDER BY obs_month) AS oil_lag3,
+        LAG(oil_idx, 4) OVER(ORDER BY obs_month) AS oil_lag4
+    FROM gold_indexed
+) AS sub
+WHERE obs_month >= '2010-05-01'; -- bulan di tentukan karna menghindari null value

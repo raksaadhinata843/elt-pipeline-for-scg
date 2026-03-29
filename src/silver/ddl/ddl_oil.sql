63cf3d724f04a010fbb762e2ca0f39c6ab4242f5
Raksa Adhinata <raksaadhinata@gmail.com>
2026-03-30 02:43:33 +0700

    Cast period and value to appropriate data types

--- src/silver/ddl/ddl_oil.sql
+++ src/silver/ddl/ddl_oil.sql
@@ -1,6 +1,6 @@
 CREATE OR REPLACE TABLE silver_oil AS
 SELECT
-  period AS date,
-  value AS oil_price
+  period::DATE AS date,
+  value::DOUBLE AS oil_price
 FROM bronze_oil
 QUALIFY row_number() OVER (PARTITION BY period ORDER BY period DESC) = 1;

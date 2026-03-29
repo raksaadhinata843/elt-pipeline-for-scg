b736d3d9df213a32cf1fdba9536dc583940ccfcf
Raksa Adhinata <raksaadhinata@gmail.com>
2026-03-30 02:28:47 +0700

    Fix SQL syntax for table creation and qualification

--- src/silver/ddl/ddl_coal.sql
+++ src/silver/ddl/ddl_coal.sql
@@ -1,6 +1,6 @@
-CREATE OR REPLACE TABLE silver_coal as
+CREATE OR REPLACE TABLE silver_coal AS
 SELECT
   date,
   value AS coal_price
 FROM bronze_coal
-qualify row_number() over (partition BY date ORDER BY realtime_start DESC) = 1;
+QUALIFY row_number() OVER (PARTITION BY date ORDER BY realtime_start DESC) = 1;

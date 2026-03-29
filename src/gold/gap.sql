7de9d57862634e7015f4f518c5e4b7ce9c465213
Raksa Adhinata <raksaadhinata@gmail.com>
2026-03-30 04:10:40 +0700

    Create gold_gap table with energy gap calculations

--- src/gold/gap.sql
+++ src/gold/gap.sql
@@ -1,1 +1,13 @@
-
+CREATE OR REPLACE TABLE gold_gap AS
+SELECT 
+    obs_month,
+    cement_idx,
+    coal_idx,
+    oil_idx,
+    -- Gap Coal (Produksi)
+    (cement_idx - coal_idx) AS gap_coal,
+    -- Gap Oil (Logistik)
+    (cement_idx - oil_idx) AS gap_oil,
+    -- Total Gap: Tekanan energi keseluruhan terhadap semen
+    (cement_idx - ((coal_idx + oil_idx) / 2)) AS gap_total_energy
+FROM gold_indexed;

fdc8f65b7905c0e6f8766d13323233ac899012cf
Raksa Adhinata <raksaadhinata@gmail.com>
2026-03-30 02:42:05 +0700

    Cast date and value to appropriate types in SQL

--- src/silver/ddl/ddl_cement.sql
+++ src/silver/ddl/ddl_cement.sql
@@ -1,6 +1,6 @@
 CREATE OR REPLACE TABLE silver_cement AS
 SELECT
-  date,
-  value AS cement_price
+  date::DATE AS date,
+  value::DOUBLE AS cement_price
 FROM bronze_cement
 QUALIFY row_number() OVER (PARTITION BY date ORDER BY realtime_start DESC) = 1;

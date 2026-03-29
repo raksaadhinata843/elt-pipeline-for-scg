fd1a8a525982a2cbd79b11db297e4da602c6a9fe
Raksa Adhinata <raksaadhinata@gmail.com>
2026-03-30 02:42:44 +0700

    Cast date and value to appropriate types in SQL

--- src/silver/ddl/ddl_coal.sql
+++ src/silver/ddl/ddl_coal.sql
@@ -1,6 +1,6 @@
 CREATE OR REPLACE TABLE silver_coal AS
 SELECT
-  date,
-  value AS coal_price
+  date::DATE AS date,
+  value::DOUBLE AS coal_price
 FROM bronze_coal
 QUALIFY row_number() OVER (PARTITION BY date ORDER BY realtime_start DESC) = 1;

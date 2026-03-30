ca0ed921d82da3d4a58c38e04a6bcda17ad98ee6
Raksa Adhinata <raksaadhinata@gmail.com>
2026-03-31 00:49:34 +0700

    Change date type from DATE to TIMESTAMP in ddl_oil.sql

--- src/silver/ddl/ddl_oil.sql
+++ src/silver/ddl/ddl_oil.sql
@@ -1,6 +1,6 @@
 CREATE OR REPLACE TABLE silver_oil AS
 SELECT
-  period::DATE AS date,
+  period::TIMESTAMP AS date,
   value::DOUBLE AS oil_price
 FROM bronze_oil
 QUALIFY row_number() OVER (PARTITION BY period ORDER BY period DESC) = 1;
